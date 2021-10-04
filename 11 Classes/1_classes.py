3d6ef56655d0059779da6c80bf3837b0bdeca1b4
Narayanan Ts <tsnarayanan7@gmail.com>
2021-10-04 16:03:43 +0530

    Classes

--- 11 Classes/1_classes.py
+++ 11 Classes/1_classes.py
@@ -14,3 +14,25 @@ print(p1.myAge)
 class Student(Person):
   def __init__(self, fname, lname):
     super().__init__(fname, lname)
+
+
+class Student:
+    def __init__(self,name,age,marks):
+        self.name = name
+        self.age = age
+        self.marks = marks 
+
+    def myMarks(self):
+        print(' You scored this', self.marks)
+
+
+p1 = Student('Narayanan',29,90)
+print(p1.myMarks())
+
+
+class Person(Student):
+    def __init__(self, name, age, marks):
+        super().__init__(name, age, marks)
+
+p2 = Person('Girija',53,99)
+print(p2.myMarks())
