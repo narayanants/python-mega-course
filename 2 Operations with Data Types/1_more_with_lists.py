2ba2980ab21dfbed5f86f00695559f7831b5c566
Narayanan Ts <tsnarayanan7@gmail.com>
2021-12-06 10:43:23 +0530

    More with lists

--- 2 Operations with Data Types/1_more_with_lists.py
+++ 2 Operations with Data Types/1_more_with_lists.py
@@ -13,3 +13,13 @@ print(temp.index(8.5,0,2))
 temp.clear()
 print(temp)
 
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+
+thislist1 = ["apple", "banana", "cherry"]
+mylist1 = thislist1.copy()
+print(mylist1)
+
