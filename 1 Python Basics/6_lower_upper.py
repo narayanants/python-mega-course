b6847d59873765dbe21fc0f344087f145a15e916
Narayanan Ts <tsnarayanan7@gmail.com>
2021-12-06 10:43:16 +0530

    Lower upper

--- 1 Python Basics/6_lower_upper.py
+++ 1 Python Basics/6_lower_upper.py
@@ -1,3 +1,11 @@
 god = 'THoR'
 print(god.lower())
 print(god.upper())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+
+
+b = "Hello, World!"
+print(b.split(",")) # returns ['Hello', ' World!']
